1a18e604277168c31d19d7c9aa6bd4c0c3bdf644
Gammern <Gammern@users.noreply.github.com>
2017-05-08 19:53:41 +0200

    Tutorial from https://docs.microsoft.com/nb-no/aspnet/core/data/ef-mvc/ (#1)
    * Generated
    
    * db
    
    * getting started done
    
    * CRUD modifications
    
    * sorting
    
    * migrations
    
    * MaxLengthOnNames migration
    
    * Col student.firstname migration
    
    * complex model
    
    * reading related data
    
    * updating related data
    
    * HireDate bug
    
    * concurrency
    
    * done inheritance
    
    * done tutorial

---
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            List<EnrollmentDateGroup> groups = new List<EnrollmentDateGroup>();
+            var conn = _context.Database.GetDbConnection();
+            try
+            {
+                await conn.OpenAsync();
+                using (var command = conn.CreateCommand())
+                {
+                    string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+                        + "FROM Person "
+                        + "WHERE Discriminator = 'Student' "
+                        + "GROUP BY EnrollmentDate";
+                    command.CommandText = query;
+
+                    using (DbDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                var row = new EnrollmentDateGroup
+                                {
+                                    EnrollmentDate = reader.GetDateTime(0),
+                                    StudentCount = reader.GetInt32(1)
+                                };
+                                groups.Add(row);
+                            }
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(groups);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
