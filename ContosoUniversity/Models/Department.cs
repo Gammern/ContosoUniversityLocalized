f1770aa4628cb7711bc1538983133adf29360e88
Johan Karl Larsen <johan@itlarsen.net>
2017-05-17 21:59:35 +0200

    localize misc. views and res files

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -10,10 +10,12 @@ namespace ContosoUniversity.Models
         public int DepartmentID { get; set; }
 
         [StringLength(50, MinimumLength = 3)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
         [DataType(DataType.Currency)]
         [Column(TypeName = "money")]
+        [Display(Name = "Budget")]
         public decimal Budget { get; set; }
 
         [DataType(DataType.Date)]
@@ -26,6 +28,7 @@ namespace ContosoUniversity.Models
         [Timestamp, Display(AutoGenerateFilter = false)]
         public byte[] RowVersion { get; set; }
 
+        [Display(Name = "Administrator")]
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
